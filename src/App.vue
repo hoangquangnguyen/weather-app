97db94768dfff5c77ab9f7e4de5be61057066161
nhquang192 <nhquang192@gmail.com>
2022-08-16 14:49:49 +0700

    init pj

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="bg-red-600 p-4 text-3xl">ASDASDA</div>
+  <router-view />
 </template>
 
 <style>
@@ -13,18 +10,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
